deebddda89b1809e19eb31eef84ac76638e2c54b
Gold <155002808+ITSCraftyMinded14@users.noreply.github.com>
2026-02-15 08:03:23 -0500

    Update index.js

--- RelayAI/index.js
+++ RelayAI/index.js
@@ -21,6 +21,7 @@ const lastUsed = {};
 
 // helper: add message
 function addMessage(text,type){
+  document.createElement("br")
   const msg = document.createElement("div");
   msg.className = "msg " + type;
   msg.innerText = text;
